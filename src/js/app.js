f2d3509d6025c82375aa6b214286d8eba5fe83fa
Alex Fonte <aramosdf@gmail.com>
2025-01-17 19:53:31 +0100

    test js defer portada

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
-import Swal from 'sweetalert2';
+document.addEventListener('DOMContentLoaded', (event) => {
 
+    const name = 'world';
+    console.log(`Hello ${name}`);
 
-const name = 'world';
-console.log(`Hello ${name}`);
+    const sweet = Swal.mixin({
+        customClass: {
+            container: "background-viana-hello",
+            popup: "background-viana-hello-popup",
+            confirmButton: "background-viana-hello-btn",
+        }
+    });
 
-const sweet = Swal.mixin({
-    customClass: {
-        container: "background-viana-hello",
-        popup: "background-viana-hello-popup",
-        confirmButton: "background-viana-hello-btn",
-    }
+    sweet.fire({ title: 'Benvingut Viana do Castelo: Una Joia Històrica i Cultural de Portugal' });
 });
-
-sweet.fire({title:'Benvingut Viana do Castelo: Una Joia Històrica i Cultural de Portugal'});
